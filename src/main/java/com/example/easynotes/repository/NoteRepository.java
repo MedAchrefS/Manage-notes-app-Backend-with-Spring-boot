ea5a1fb475d157893c6e65d1e4adb218121083f5
achref5 <med.achref.sakkat@gmail.com>
2019-04-01 19:33:32 +0100

    First commit

---
+++ src/main/java/com/example/easynotes/repository/NoteRepository.java
@@ -0,0 +1,12 @@
+package com.example.easynotes.repository;
+
+import com.example.easynotes.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+@CrossOrigin(origins = "http://localhost:4200")
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
